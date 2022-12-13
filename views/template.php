8bc27ea7f05a1557b67f14d8b45915fda88f112f
MafeBlandon <119081057+MafeBlandon@users.noreply.github.com>
2022-12-13 07:57:09 -0500

    agregue los archivos

---
+++ views/template.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LARCEI| JUEGO VIRTUAL</title>
+
+	<link rel="icon" href="views/img/intro/larcei.png">
+	<link href="views/css/estilo.css" type="text/css" rel="stylesheet" media="">
+	<link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+	<!-- font-family: 'Patrick Hand', cursive; -->
+
+	<script src="views/js/screenfull.min.js"></script>
+
+</head>
+<body>
+
+	<!--=====================================
+	PANTALLA VERTICAL
+	======================================-->
+
+	<div id="vertical"></div>
+
+	<!--=====================================
+	MARCO
+	======================================-->
+
+	<div id="marco"></div>
+
+	<!--=====================================
+	CONTENEDOR
+	======================================-->
+
+	<div id="contenedor">
+		
+		<?php
+
+			if(isset($_GET["validar"])){
+
+				switch($_GET["validar"]){
+					case "inicio":
+					include "modules/inicio.php";
+					break;
+					case "salir":
+					include "modules/salir.php";
+					break;
+					default:
+					include "modules/ingreso.php";
+				}
+
+			}else{
+
+				include "modules/ingreso.php";
+			
+			}
+	
+		?>
+
+	</div>
+
+	<!--=====================================
+	CREDITOS
+	======================================-->
+
+	<footer>
+		
+		<center>
+		<p>Juego desarrollado por Ana Isabel Montes Y Mafe Blandon| <a href="https://lasMasLindas.com" target="_blank">www.LasMasLindas.com</a></p>
+		</center>
+
+	</footer>
+
+<script src="views/js/variables_y_propiedades.js"></script>
+<script src="views/js/inicio.js"></script>
+<script src="views/js/juego.js"></script>
+<script src="views/js/lienzo.js"></script>
+<script src="views/js/ampliarCanvas.js"></script>
+</body>
+</html>
